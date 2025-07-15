a70cc110f24366712dd70e766b6044ba6c981de7
AlexNdungu <alexmeta517@gmail.com>
2025-07-15 09:56:49 +0300

    send emails

--- Email/settings.py
+++ Email/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -129,3 +132,11 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ["EMAIL_HOST_USER"]
+EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
--- Email1/views.py
+++ Email1/views.py
@@ -1,4 +1,11 @@
+import os
 from django.shortcuts import render
+from django.core.mail import send_mail
+from django.http import JsonResponse
+from smtplib import SMTPException
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Create your views here.
 
@@ -11,3 +18,21 @@ def send_email(request):
         message = request.POST.get('message')
         print(subject)
         print(message)
+        try:
+            result = send_mail(
+                subject=subject,
+                message=message,
+                from_email= os.environ["EMAIL_HOST_USER"],
+                recipient_list=['alexmeta517@gmail.com'],
+                fail_silently=False,
+            )
+
+            if result == 1:
+                return JsonResponse({'status': 'success', 'message': 'Email sent successfully'})
+            else:
+                return JsonResponse({'status': 'failed', 'message': 'Email not sent'})
+
+        except SMTPException as e:
+            return JsonResponse({'status': 'error', 'message': f'SMTP error: {str(e)}'})
+        except Exception as e:
+            return JsonResponse({'status': 'error', 'message': f'Unexpected error: {str(e)}'})
