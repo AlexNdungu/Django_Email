f9760abc1c552c1610064e654d16129f5487ec40
AlexNdungu <alexmeta517@gmail.com>
2025-07-15 09:21:56 +0300

    send emails

--- Static/Js/Home.js
+++ Static/Js/Home.js
@@ -1,1 +1,8 @@
-console.log('home');
+let csrf = document.getElementsByName('csrfmiddlewaretoken');
+let email_subject = document.getElementById('email_subject');
+let email_message = document.getElementById('getElementById');
+let send_email_button = document.getElementById('send_email_button');
+
+send_email_button.addEventListener('click', ()=> {
+
+});
